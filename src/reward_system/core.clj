4e5bc18c70e094b5efa6eff88a4ceb79f1e66f43
Bruno Bacarini <bacarini.bruno@gmail.com>
2016-04-27 18:51:02 -0300

    starting to play with matrix in the core module

--- src/reward_system/core.clj
+++ src/reward_system/core.clj
@@ -19,15 +19,36 @@
                 :predecessor (last (remove nil? (find_predecessor (first ids))))
               }] node ))
 
-(defn load-graph
+(defn all_customers
+  [array_str]
+  (let [array (flatten
+                (into []
+                  (map (fn [line] (re-seq #"\S+" line)) array_str)))]
+    (map #(Integer/parseInt %) array)))
+
+(defn load_matrix
+  [graph max_customer]
+  (println \newline "graph nodes:" graph)
+  (def m (matrix/create max_customer max_customer))
+  (let [result-array
+          (into [] (for [ii (range max_customer)]
+            (into [] (for [jj (range max_customer)]
+              (def m (matrix/add_score m ii jj (+ ii jj)))))))
+  ] m ))
+
+(defn load_graph
   [file-str]
-  (let [file-lines (str/split-lines file-str) ]
-    (doseq [line-str file-lines]
-      (let [ids      (re-seq #"\S+" line-str) ]
+  (let [file_lines (str/split-lines file-str) ]
+    (def max_customer (apply max (set (all_customers file_lines))))
+    (doseq [line_str file_lines]
+      (let [ids      (re-seq #"\S+" line_str) ]
         (assert (= 2 (count ids)))
         (swap! graph update-in [:graph] conj (build_hash ids))))))
 
 (defn -main []
   (let [text  (slurp "input.txt") ]
-    (load-graph text)
-    (println \newline "graph nodes:" (@graph :graph))))
+    (load_graph text)
+    (let [matrix (load_matrix (@graph :graph) max_customer)]
+      (println matrix))))
+
+
