83f97154a7c5925abac08bbc81115b7ae52c22ac
Bruno Bacarini <bacarini.bruno@gmail.com>
2016-04-29 01:06:26 -0300

    improvements on method to load graph and matrix, thus new method to manager input file

--- src/reward_system/core.clj
+++ src/reward_system/core.clj
@@ -6,6 +6,15 @@
 (def graph
   (atom { :graph nil }))
 
+(defn load_matrix [graph matrix]
+  (if (= 1 (count graph))
+    (matrix/add_score matrix  (- (Integer/parseInt (get (first graph) :customer)) 1)
+                              (- (Integer/parseInt (get (first graph) :friend)) 1)
+                              (+ 1 1))
+    (recur (rest graph) (matrix/add_score matrix (- (Integer/parseInt (get (first graph) :customer)) 1)
+                                                 (- (Integer/parseInt (get (first graph) :friend)) 1)
+                                                 (+ 1 1)))))
+
 (defn find_predecessor
   [search]
   (for [ node (into [] (get @graph :graph)) ]
@@ -13,42 +22,35 @@
       (get-in node [:customer]))))
 
 (defn build_hash
-  [ids]
-  (let [ node { :customer (first ids)
-                :friend (second ids)
-                :predecessor (last (remove nil? (find_predecessor (first ids))))
+  [customer friend]
+  (let [ node { :customer customer
+                :friend friend
+                :predecessor (last (remove nil? (find_predecessor customer)))
               }] node ))
 
-(defn all_customers
-  [array_str]
-  (let [array (flatten
-                (into []
-                  (map (fn [line] (re-seq #"\S+" line)) array_str)))]
-    (map #(Integer/parseInt %) array)))
-
-(defn load_matrix
-  [graph max_customer]
-  (println \newline "graph nodes:" graph)
-  (def m (matrix/create max_customer max_customer))
-  (let [result-array
-          (into [] (for [ii (range max_customer)]
-            (into [] (for [jj (range max_customer)]
-              (def m (matrix/add_score m ii jj (+ ii jj)))))))
-  ] m ))
-
 (defn load_graph
-  [file-str]
-  (let [file_lines (str/split-lines file-str) ]
-    (def max_customer (apply max (set (all_customers file_lines))))
-    (doseq [line_str file_lines]
-      (let [ids      (re-seq #"\S+" line_str) ]
-        (assert (= 2 (count ids)))
-        (swap! graph update-in [:graph] conj (build_hash ids))))))
+  [invitations]
+    (doseq [[customer friend] invitations]
+      (swap! graph update-in [:graph] conj (build_hash customer friend))))
+
+(defn split_line
+  [line_str]
+    (let [ids (re-seq #"\S+" line_str) ]
+      (assert (= 2 (count ids)))
+      ids))
+
+(defn read_file
+  [file_name]
+    (map #(split_line %) (str/split-lines (slurp file_name))))
+
+(defn max_customer
+  [invitations]
+  (apply max (map #(Integer/parseInt %) (set (flatten invitations)))))
 
 (defn -main []
-  (let [text  (slurp "input.txt") ]
-    (load_graph text)
-    (let [matrix (load_matrix (@graph :graph) max_customer)]
-      (println matrix))))
-
-
+    (let [invitations (read_file "input.txt")
+          max_customer (max_customer invitations)
+        ]
+      (load_graph invitations)
+      (load_matrix (reverse (@graph :graph))
+                   (matrix/create max_customer max_customer))))
