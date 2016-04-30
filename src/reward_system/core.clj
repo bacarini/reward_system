cc9111f288c502afe3d0d7aba5b61c6ed729191f
Bruno Bacarini <bacarini.bruno@gmail.com>
2016-04-30 11:47:28 -0300

    add methods documentations

--- src/reward_system/core.clj
+++ src/reward_system/core.clj
@@ -5,18 +5,27 @@
     [reward_system.matrix :as matrix]))
 
 (def state
+ ^{ :doc "Creates a new atom with graph and sorted-set that
+     represents all customers that are already invited" }
   (atom {
     :graph nil
     :invited (sorted-set)
   }))
 
 (defn select_predecessor
+  "Selects the predecessor of specific node.
+
+  Returns a node."
   [search]
   (last (filter (fn [node]
           (= search (get node :customer)))
           (@state :graph))))
 
 (defn setting_value_to_matrix
+  "It sets recursively value into matrix.
+  Once given the node, this method finds all the predecessors and set value for them as well.
+
+  Returns a matrix (a vector of vectors)"
   ([matrix node]
     (setting_value_to_matrix matrix node 0))
   ([matrix node score]
@@ -36,18 +45,28 @@
                              (- score 1)))))
 
 (defn load_matrix
+  "Loads matrix according to a given graph.
+
+  Returns a matrix (a vector of vectors)"
   ([graph matrix]
   (if (= 1 (count graph))
     (setting_value_to_matrix matrix (first graph))
     (recur (rest graph) (setting_value_to_matrix matrix (first graph))))))
 
 (defn find_predecessor
+  "Finds the predecessor of a given value (:friend).
+
+  Returns the predecessor (:customer)"
   [search]
   (for [ node (into [] (get @state :graph)) ]
     (if (= (get-in node [:friend]) search)
       (get-in node [:customer]))))
 
-(defn build_hash
+(defn build_node
+  "It builds the node.
+
+  Returns a node
+  Ex. {:customer '2' :friend '3' :predecessor '1'}"
   [customer friend]
   (let [ node { :customer customer
                 :friend friend
@@ -55,40 +74,51 @@
               }] node ))
 
 (defn load_graph
+  "Loads a graph from a list of given invitations.
+
+  Does not return any value."
   [invitations]
     (doseq [[customer friend] invitations]
-      (swap! state update-in [:graph] conj (build_hash customer friend))))
+      (swap! state update-in [:graph] conj (build_node customer friend))))
 
 (defn split_line
+  "Given a string with 2 ids, customer and friend in this context, splits into a list.
+
+  Returns a list of ids
+  Ex.('1' '3')"
   [line_str]
     (let [ids (re-seq #"\S+" line_str) ]
       (assert (= 2 (count ids)))
       ids))
 
 (defn read_file
+  "Reads file and returns a list of invitations.
+
+  Ex. (('1' '3') ('1' '2') ('3' '4') ('2' '4') ('4' '5') ('4' '6'))"
   [file_name]
     (map #(split_line %) (str/split-lines (slurp file_name))))
 
-(defn max_customer
-  [invitations]
-  (apply max (map #(Integer/parseInt %) (set (flatten invitations)))))
-
 (defn get_values_from_customer
+  "Finds the score to a given customer."
   [customer matrix]
     (hash-map customer (matrix/sum_scores matrix (- (Integer/parseInt customer) 1))))
 
-(defn all_customers
+(defn all_involved
+  "Returns everyone involved in the process of invitation,
+  no matter if they are friends or customers"
   [graph]
   (s/union
     (set (into [] (map (fn [node] (get node :customer)) graph)))
     (set (into [] (map (fn [node] (get node :friend)) graph)))))
 
 (defn ranking
+  "Returns the customer ranking of invitations process."
   [matrix]
     (sort-by val >
-      (into {} (map #(get_values_from_customer % matrix) (all_customers (@state :graph))))))
+      (into {} (map #(get_values_from_customer % matrix) (all_involved (@state :graph))))))
 
 (defn reset
+  "Resets the atom."
   []
   (swap! state assoc-in [:graph]   nil)
   (swap! state assoc-in [:invited] (sorted-set)))
@@ -96,9 +126,9 @@
 (defn -main []
     (reset)
     (let [invitations (read_file "input.txt")
-          max_customer (max_customer invitations)
+          count_customer (count (set (flatten invitations)))
         ]
       (load_graph invitations)
       (println (ranking
                   (load_matrix (reverse (@state :graph))
-                     (matrix/create max_customer max_customer))))))
+                     (matrix/create count_customer))))))
